4ea0e1d9870dfcc13dcd5462d17a667d0c58094a
Tim Wright <tim@tfwright.co.nz>
2020-03-29 15:06:58 +1300

    We now show a prompt

--- src/exercise_02/main_test.go
+++ src/exercise_02/main_test.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"io"
+	"strings"
 	"testing"
 )
 
 func TestCanary(t *testing.T) {
 	assert.Equal(t, true, true)
+}
+
+func TestPrompt(t *testing.T) {
+	reader := io.Reader(strings.NewReader("Tim\n"))
+	writer := new(bytes.Buffer)
+
+	EchoInputStringLength(reader, writer)
+
+	actual := string(writer.Bytes())
+	assert.Contains(t, actual, "What is the input string? ")
+}
+
+func EchoInputStringLength(reader io.Reader, writer io.Writer) {
+	fmt.Fprint(writer, "What is the input string? ")
 }
