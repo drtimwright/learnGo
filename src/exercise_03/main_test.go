156f93e8afc567ffe742d7528a116cde3a685d98
Tim Wright <tim@tfwright.co.nz>
2020-03-29 15:20:07 +1300

    We now prompt for teh author

--- src/exercise_03/main_test.go
+++ src/exercise_03/main_test.go
@@ -12,13 +12,14 @@ import (
 func EchoQuote(reader io.Reader, writer io.Writer) {
 
 	fmt.Fprint(writer, "What is the quote? ")
+	fmt.Fprint(writer, "Who said it? ")
 }
 
 func TestCanary(t *testing.T) {
 	assert.Equal(t, true, true)
 }
 
-func TestPrompt(t *testing.T) {
+func TestQuotePrompt(t *testing.T) {
 	reader := io.Reader(strings.NewReader("Quote\nTim\n"))
 	writer := new(bytes.Buffer)
 
@@ -26,5 +27,15 @@ func TestPrompt(t *testing.T) {
 
 	actual := string(writer.Bytes())
 	assert.Contains(t, actual, "What is the quote? ")
+}
+
+func TestAuthorPrompt(t *testing.T) {
+	reader := io.Reader(strings.NewReader("Quote\nTim\n"))
+	writer := new(bytes.Buffer)
+
+	EchoQuote(reader, writer)
+
+	actual := string(writer.Bytes())
+	assert.Contains(t, actual, "Who said it? ")
 }
 
