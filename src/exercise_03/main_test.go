a67d4f3037024eb2c4575c86002b2833dc740cf5
Tim Wright <tim@tfwright.co.nz>
2020-03-29 15:19:27 +1300

    We now print the quote prompt

--- src/exercise_03/main_test.go
+++ src/exercise_03/main_test.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"io"
+	"strings"
 	"testing"
 )
+
+func EchoQuote(reader io.Reader, writer io.Writer) {
+
+	fmt.Fprint(writer, "What is the quote? ")
+}
 
 func TestCanary(t *testing.T) {
 	assert.Equal(t, true, true)
 }
+
+func TestPrompt(t *testing.T) {
+	reader := io.Reader(strings.NewReader("Quote\nTim\n"))
+	writer := new(bytes.Buffer)
+
+	EchoQuote(reader, writer)
+
+	actual := string(writer.Bytes())
+	assert.Contains(t, actual, "What is the quote? ")
+}
+
