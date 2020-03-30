09bc5eef9586e8570d3b79cc5fea035a243a1867
Tim Wright <tim@tfwright.co.nz>
2020-03-30 20:46:37 +1300

    move addition to a method on struct

--- src/exercise_05/main_test.go
+++ src/exercise_05/main_test.go
@@ -16,7 +16,10 @@ import (
 type SimpleMathData struct {
 	Num1  int64
 	Num2  int64
-	Added int64
+}
+
+func (sm SimpleMathData) Added() int64 {
+	return sm.Num1 + sm.Num2
 }
 
 func SimpleMath(reader io.Reader, writer *bytes.Buffer) {
@@ -24,7 +27,7 @@ func SimpleMath(reader io.Reader, writer *bytes.Buffer) {
 	firstNumber, _ := strconv.ParseInt(libs.GetPromptedString(writer, bufReader, "What is the first number? "), 10, 64)
 	secondNumber, _ := strconv.ParseInt(libs.GetPromptedString(writer, bufReader, "What is the second number? "), 10, 64)
 
-	data := SimpleMathData{firstNumber, secondNumber, firstNumber + secondNumber}
+	data := SimpleMathData{firstNumber, secondNumber}
 
 	templ, _ := template.New("simple math template").Parse("{{.Num1}} + {{.Num2}} = {{.Added}}\n")
 	templ.Execute(writer, data)
