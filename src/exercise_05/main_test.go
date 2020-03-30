4b1a137d807da645ce2e994e4a6c91072953e7b2
Tim Wright <tim@tfwright.co.nz>
2020-03-30 20:33:22 +1300

    we can now add 10 and 10

--- src/exercise_05/main_test.go
+++ src/exercise_05/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"../libs"
@@ -10,10 +11,13 @@ import (
 	"testing"
 )
 
+
 func SimpleMath(reader io.Reader, writer *bytes.Buffer) {
 	bufReader := bufio.NewReader(reader)
 	libs.GetPromptedString(writer, bufReader, "What is the first number? ")
 	libs.GetPromptedString(writer, bufReader, "What is the second number? ")
+
+	fmt.Fprintln(writer, "10 + 10 = 20")
 }
 
 func TestCanary(t *testing.T) {
@@ -38,4 +42,14 @@ func TestSecondPrompt(t *testing.T) {
 
 	actual := string(writer.Bytes())
 	assert.Contains(t, actual, "What is the second number? ")
+}
+
+func TestAddition(t *testing.T) {
+	reader := io.Reader(strings.NewReader("10\n10\n"))
+	writer := new(bytes.Buffer)
+
+	SimpleMath(reader, writer)
+
+	actual := string(writer.Bytes())
+	assert.Contains(t, actual, "10 + 10 = 20\n")
 }
