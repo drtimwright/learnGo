2390e4e607979ecc42b2f39c5fe4f9b395e9848d
Tim Wright <tim@tfwright.co.nz>
2020-03-30 20:06:29 +1300

    we now display ajectives

--- src/exercise_04/main_test.go
+++ src/exercise_04/main_test.go
@@ -16,8 +16,9 @@ func MadLib(reader io.Reader, writer *bytes.Buffer) {
 
 	noun := libs.GetPromptedString(writer, bufReader, "Enter a noun: ")
 	verb := libs.GetPromptedString(writer, bufReader, "Enter a verb: ")
+	adjective := libs.GetPromptedString(writer, bufReader, "Enter an adjective: ")
 
-	fmt.Fprintln(writer, "Do you", verb, "your blue", noun, "quickly? That's hilarious")
+	fmt.Fprintln(writer, "Do you", verb, "your", adjective, noun, "quickly? That's hilarious")
 }
 
 func TestCanary(t *testing.T) {
@@ -55,7 +56,7 @@ func TestOutputNoun(t *testing.T) {
 }
 
 func TestVerbPrompt(t *testing.T) {
-	reader := io.Reader(strings.NewReader("cat\nrun\nblue\nquickly\n"))
+	reader := io.Reader(strings.NewReader("dog\nrun\nblue\nquickly\n"))
 	writer := new(bytes.Buffer)
 
 	MadLib(reader, writer)
@@ -65,11 +66,31 @@ func TestVerbPrompt(t *testing.T) {
 }
 
 func TestOutputVerb(t *testing.T) {
-	reader := io.Reader(strings.NewReader("cat\nrun\nblue\nquickly\n"))
+	reader := io.Reader(strings.NewReader("dog\nrun\nblue\nquickly\n"))
 	writer := new(bytes.Buffer)
 
 	MadLib(reader, writer)
 
 	actual := string(writer.Bytes())
-	assert.Contains(t, actual, "Do you run your blue cat quickly? That's hilarious")
+	assert.Contains(t, actual, "Do you run your blue dog quickly? That's hilarious")
+}
+
+func TestAdjectivePrompt(t *testing.T) {
+	reader := io.Reader(strings.NewReader("dog\nwalk\nred\nquickly\n"))
+	writer := new(bytes.Buffer)
+
+	MadLib(reader, writer)
+
+	actual := string(writer.Bytes())
+	assert.Contains(t, actual, "Enter an adjective: ")
+}
+
+func TestOutputAdjective(t *testing.T) {
+	reader := io.Reader(strings.NewReader("dog\nwalk\nred\nquickly\n"))
+	writer := new(bytes.Buffer)
+
+	MadLib(reader, writer)
+
+	actual := string(writer.Bytes())
+	assert.Contains(t, actual, "Do you walk your red dog quickly? That's hilarious")
 }
