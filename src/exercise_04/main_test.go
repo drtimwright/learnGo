73e5cdefb680e4e4120589724b6de347d0397b65
Tim Wright <tim@tfwright.co.nz>
2020-03-30 19:54:33 +1300

    We now print the right string for walk dog blue quickly

--- src/exercise_04/main_test.go
+++ src/exercise_04/main_test.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"io"
+	"strings"
 	"testing"
 )
 
+func MadLib(reader io.Reader, writer *bytes.Buffer) {
+
+	fmt.Fprintln(writer, "Do you walk your blue dog quickly? That's hilarious")
+}
+
 func TestCanary(t *testing.T) {
 	assert.Equal(t, true, true)
+}
+
+func TestOutput1(t *testing.T) {
+	reader := io.Reader(strings.NewReader("dog\nwalk\nblue\nquickly\n"))
+	writer := new(bytes.Buffer)
+
+	MadLib(reader, writer)
+
+	actual := string(writer.Bytes())
+	assert.Contains(t, actual, "Do you walk your blue dog quickly? That's hilarious")
 }
