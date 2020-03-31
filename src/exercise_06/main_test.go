8cc08dd9d638f1bc0ea617b5cf39e75d28878854
Tim Wright <tim@tfwright.co.nz>
2020-03-31 19:44:46 +1300

    we now use the retirement age in the calculation

--- src/exercise_06/main_test.go
+++ src/exercise_06/main_test.go
@@ -18,8 +18,9 @@ func RetirementCalculator(reader io.Reader, writer *bytes.Buffer, currentYear in
 	bufReader := bufio.NewReader(reader);
 
 	currentAge, _ := libs.GetPromptedNumber(writer, bufReader, "")
+	retirementAge, _ := libs.GetPromptedNumber(writer, bufReader, "")
 
-	numYears := 65 - currentAge
+	numYears := retirementAge - currentAge
 
 	fmt.Fprintln(writer, "You have", numYears, "years left until you can retire.")
 }
@@ -39,8 +40,18 @@ func Test40YearsLeft(t *testing.T) {
 }
 
 
-func Test30YearsLeft(t *testing.T) {
+func TestUseCurrentAgeInComputation(t *testing.T) {
 	reader := io.Reader(strings.NewReader("35\n65\n"))
+	writer := new(bytes.Buffer)
+
+	RetirementCalculator(reader, writer, 2018)
+
+	actual := string(writer.Bytes())
+	assert.Contains(t, actual, "You have 30 years left until you can retire.\n")
+}
+
+func TestUseRetirementAgeInComputation(t *testing.T) {
+	reader := io.Reader(strings.NewReader("25\n55\n"))
 	writer := new(bytes.Buffer)
 
 	RetirementCalculator(reader, writer, 2018)
