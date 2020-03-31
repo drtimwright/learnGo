1484f3d086081a11907f1bbdc0ead6cfcdea4c65
Tim Wright <tim@tfwright.co.nz>
2020-03-31 19:18:53 +1300

    We now use the current age in the calculation

--- src/exercise_06/main_test.go
+++ src/exercise_06/main_test.go
@@ -1,10 +1,50 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"io"
+	"strings"
 	"testing"
+	"../libs"
 )
+
+
+
+func RetirementCalculator(reader io.Reader, writer *bytes.Buffer, currentYear int64) {
+
+	bufReader := bufio.NewReader(reader);
+
+	currentAge, _ := libs.GetPromptedNumber(writer, bufReader, "")
+
+	numYears := 65 - currentAge
+
+	fmt.Fprintln(writer, "You have", numYears, "years left until you can retire.")
+}
 
 func TestCanary(t *testing.T) {
 	assert.Equal(t, true, true)
+}
+
+func Test40YearsLeft(t *testing.T) {
+	reader := io.Reader(strings.NewReader("25\n65\n"))
+	writer := new(bytes.Buffer)
+
+	RetirementCalculator(reader, writer, 2018)
+
+	actual := string(writer.Bytes())
+	assert.Contains(t, actual, "You have 40 years left until you can retire.\n")
+}
+
+
+func Test30YearsLeft(t *testing.T) {
+	reader := io.Reader(strings.NewReader("35\n65\n"))
+	writer := new(bytes.Buffer)
+
+	RetirementCalculator(reader, writer, 2018)
+
+	actual := string(writer.Bytes())
+	assert.Contains(t, actual, "You have 30 years left until you can retire.\n")
 }
--- src/libs/prompts.go
+++ src/libs/prompts.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strconv"
 	"strings"
 )
 
@@ -11,4 +12,9 @@ func GetPromptedString(w io.Writer, reader *bufio.Reader, prompt string) string 
 	fmt.Fprint(w, prompt)
 	quote, _ := reader.ReadString('\n')
 	return strings.Trim(quote, "\n")
+}
+
+func GetPromptedNumber(w io.Writer, reader *bufio.Reader, prompt string) (int64, error) {
+	readString := GetPromptedString(w, reader, prompt)
+	return strconv.ParseInt(readString, 10, 64)
 }
