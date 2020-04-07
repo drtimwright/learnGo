1ac14ad9dbf3da660cad6c9148e39303053cf4a8
Tim Wright <tim@tfwright.co.nz>
2020-04-07 19:16:12 +1200

    we now read input variables

--- src/exercise_08/main_test.go
+++ src/exercise_08/main_test.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"strings"
 	"testing"
+	"../libs"
 )
 
 func SlicesOfPizza(reader io.Reader, writer io.Writer) {
+	bufReader := bufio.NewReader(reader)
 
-	fmt.Fprint(writer, "How many people? ")
-	fmt.Fprint(writer, "How many pizzas do you have? ")
+	numPeople, _ := libs.GetPromptedNumber(writer, bufReader, "How many people? ")
+	numPizzas, _ := libs.GetPromptedNumber(writer, bufReader, "How many pizzas do you have? ")
+
 	fmt.Fprintln(writer, "")
-	fmt.Fprintln(writer, "8 people with 2 pizzas")
+	fmt.Fprintln(writer, numPeople, "people with", numPizzas, "pizzas")
 	fmt.Fprintln(writer, "Each person gets 2 pieces of pizza.")
 	fmt.Fprintln(writer, "There are 0 leftover pieces.")
 }
@@ -33,6 +37,20 @@ func TestSampleFromChapter(t *testing.T) {
 	assert.Contains(t, actual, "How many people? ")
 	assert.Contains(t, actual, "How many pizzas do you have? ")
 	assert.Contains(t, actual, "8 people with 2 pizzas\n")
+	assert.Contains(t, actual, "Each person gets 2 pieces of pizza.\n")
+	assert.Contains(t, actual, "There are 0 leftover pieces.\n")
+}
+
+func TestDiffNumPizzas(t *testing.T) {
+	reader := io.Reader(strings.NewReader("4\n1\n"))
+	writer := new(bytes.Buffer)
+
+	SlicesOfPizza(reader, writer)
+
+	actual := string(writer.Bytes())
+	assert.Contains(t, actual, "How many people? ")
+	assert.Contains(t, actual, "How many pizzas do you have? ")
+	assert.Contains(t, actual, "4 people with 1 pizzas\n")
 	assert.Contains(t, actual, "Each person gets 2 pieces of pizza.\n")
 	assert.Contains(t, actual, "There are 0 leftover pieces.\n")
 }
